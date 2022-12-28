cee079c83e1bb5819d5a7744bb17e83e7c9b8b52
luis2003 <luis2003@gmail.com>
2022-12-28 12:50:55 -0300

    [feat] Make script loop and get new prices to compare after wait period and print prices that increased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Python script for Chuku (notification for prices changes on Binance)
 import requests
+import time
 
 
 def get_prices_usdt(url):
@@ -14,8 +15,22 @@ def get_prices_usdt(url):
 
 if __name__ == '__main__':
     api_url = 'https://api.binance.com/api/v3/ticker/price'
-    usdt_prices = get_prices_usdt(api_url)
+    usdt_prices_a = get_prices_usdt(api_url)
+    ''' # PRINTING LOGIC
     for item in usdt_prices:
         symbol = item['symbol']
         price = item['price']
         print(f'Symbol: {symbol} - Price: {price}')
+    '''
+    while True:
+        time.sleep(3)
+        usdt_prices_b = get_prices_usdt(api_url)
+        # compare logic
+        for item_a in usdt_prices_a:
+            for item_b in usdt_prices_b:
+                if item_a['symbol'] == item_b['symbol'] and item_a['price'] < item_b['price']:
+                    symbol = item_a['symbol']
+                    price = item_a['price']
+                    higher_price = item_b['price']
+                    print(f'Symbol: {symbol} - Price: {price} - Higher Price: {higher_price}')
+        print('-----')
