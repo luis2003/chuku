0ca16a84449289b63c46b17e6fd56a9d71e56f94
luis2003 <luis2003@gmail.com>
2022-12-28 11:08:27 -0300

    Remove template comments and rename function to match behavior

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-# Python script for chuku
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Python script for Chuku (notification for prices changes on Binance)
 import requests
 
 
-def print_ethbtc_price(url):
-    # Use a breakpoint in the code line below to debug your script.
+def get_prices_usdt(url):
     response = requests.get(url)
     json_data = response.json()
     for k in json_data:
-        #if k['symbol'] == 'ETHBTC':
         if 'USDT' in k['symbol']:
             symbol = k['symbol']
             price = k['price']
             print(f'Symbol: {symbol} - Price: {price}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     api_url = 'https://api.binance.com/api/v3/ticker/price'
-    print_ethbtc_price(api_url)
+    get_prices_usdt(api_url)
