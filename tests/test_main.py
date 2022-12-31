4738820dcc230fc4482b51cdb6282c70f755a451
luis2003 <luis2003@gmail.com>
2022-12-31 10:42:04 -0300

    [tests][fix] rename main module and remove invalid unit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,11 @@
-from main import get_prices_usdt, get_dict_prices_usdt, get_higher_prices, get_higher_percentage_prices
+from chuku import get_dict_prices_usdt, get_higher_percentage_prices
 
 api_url = 'https://api.binance.com/api/v3/ticker/price'
-
-
-def test_get_prices_usdt():
-    result = get_prices_usdt(api_url)
-    assert isinstance(result, list)
 
 
 def test_get_dict_prices_usdt():
     result = get_dict_prices_usdt(api_url)
     assert isinstance(result, dict)
-
-
-def test_get_higher_prices_one_price():
-    dict1 = {'a': 1, 'b': 1, 'c': 1}
-    dict2 = {'a': 1, 'b': 2, 'c': 1}
-    result = get_higher_prices(dict1, dict2)
-    assert isinstance(result, dict)
-    assert len(result) == 1
 
 
 def test_get_higher_percentage_prices_zero_price():
